0486722928b125c43801cfd81e3b747ca1f97045
ip <ipanin@users.noreply.github.com>
2019-12-11 00:45:57 +0300

    draw bitmap

--- day08/08.py
+++ day08/08.py
@@ -1,8 +1,8 @@
 # AoC 2019 Day 8: Space Image Format
-
 import util
+from PIL import Image
 
-def draw_image(image, w, h):
+def draw_text_image(image, w, h):
     pos = 0
     for _ in range(h):
         row = ""
@@ -13,16 +13,28 @@ def draw_image(image, w, h):
             elif c == 1:
                 row += '#'
             elif c == 0:
-                row += '.'
+                row += '-'
             pos += 1
         print(row)
 
+def draw_image(image, w, h):
+    img = Image.new('RGB', (w, h), "blue") # Create a new black image
+    pixels = img.load() # Create the pixel map
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            c = image[y*w + x]
+            if c == 0: # black
+                pixels[x, y] = (0, 0, 0)
+            if c == 1: # white
+
+                pixels[x, y] = (255,255,255)
+
+    img.resize((w*50, h*50)).show()
 # -----------------------------------------
 
-w = 25
-h = 6
-data = util.load_int_line('08_input.txt')
-layers = list(util.chunks(data, w*h))
+w, h = 25, 6
+input = util.load_number_string_list('08_input.txt')
+layers = list(util.chunks(input, w*h))
 
 min_zero_layer = min(layers, key = lambda layer: layer.count(0))
 ones = min_zero_layer.count(1)
@@ -39,4 +51,6 @@ for i in reversed(range(start_layer)):
         if pixel != 2: # transparent
             image[j] = pixel
 
+# What message is produced after decoding your image?
+draw_text_image(image, w, h) # actual EHRUE
 draw_image(image, w, h) # actual EHRUE
