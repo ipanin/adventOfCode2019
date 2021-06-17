aeb7b20aeb38520fe8f7637cefd42a513f2cfa75
Ilya <ipanin@users.noreply.github.com>
2021-06-17 23:23:16 +0300

    template added

---
+++ template/1.py
@@ -0,0 +1,22 @@
+# AoC 2019. Day 13.
+import util
+
+
+def test1(data, expected):
+    result = 0
+    if result != expected:
+        print(f"Error, expected={expected}, actual={result}")
+    else:
+        print("OK")
+
+
+
+#print("Part 1.")
+#data = util.load_int_list('input.txt')
+#test1(data, 179)
+
+#print("Part 2.")
+
+
+
+
